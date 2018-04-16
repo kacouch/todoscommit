a1d694148fad109db68a2fd286d2f02752b64a3e
Jaromir Pirko <jpirko@chemaxon.com>
2018-04-16 16:58:22 +0200

    Now let's handle SET_VISIBILITY_FILTER. All it needs to do is to change visibilityFilter on the state. Easy

--- src/todoApp.js
+++ src/todoApp.js
@@ -11,8 +11,13 @@ const initialState = {
 }
 ​
 function todoApp(state = initialState, action) {
-    // For now, don't handle any actions
-    // and just return the state given to us.
-    return state
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return Object.assign({}, state, {
+                visibilityFilter: action.filter
+            })
+        default:
+            return state
+    }
 }
 export default todoApp;
